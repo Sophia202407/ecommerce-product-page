f1cf5f427329b76273b057d5b08c111ae6a47880
Sophia202407 <sophiawanghongxia@gmail.com>
2025-09-08 20:05:11 +0800

    Update App.jsx
    Update App.jsx for demo test on CI/CD pipepline.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   // const unUsedvariable="example of an unused variable" // this is used to test CI error
   return (
     <>
-      <h1>Hello World from Sophia again!</h1>
+      <h1>Hello World from Sophia!</h1>
+      <p>Tonight is doing CI/CD pipeline.</p>
       <ProductPage />
       
     </>
